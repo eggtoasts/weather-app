2ebd4fd24e09923626b21a9ab8b79eadd9473353
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2025-10-08 18:40:19 -0400

    Add env.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./styles.css";
+import { API_KEY } from "./env";
+
+//fetches api
+
+let query = "orlando";
+
+let apiLink = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}?unitGroup=us&key=${API_KEY}`;
+
+async function getWeather() {
+  const getCity = await fetch(apiLink);
+  const data = await getCity.json();
+  console.log(data);
+}
+
+getWeather();
+
+//also add loading feature

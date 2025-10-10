e8bb3b6883a2d8480abab5193182a4f8c20f9bfb
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2025-10-09 22:22:53 -0400

    Add weather info component cards and fix styling -- Created a compass, UV Index, and Air Quality visual -- Added headings and initial items for daily and hourly weather interfaces -- Created a method compassLineMaker that creates the lines surrounding the compass

--- src/index.js
+++ src/index.js
@@ -16,3 +16,43 @@ async function getWeather() {
 // getWeather();
 
 console.log(window.screen);
+
+//compass line maker
+
+const far = 55;
+
+const windCircle = document.querySelector(".wind-circle");
+
+compassLineMaker(far);
+
+async function compassLineMaker(far) {
+  for (let i = 0; i < 360; i += 5) {
+    const currLine = makeLine();
+
+    if (checkIfLineShouldBePlaced(i)) continue;
+    await new Promise((resolve, reject) => setTimeout(resolve, 100));
+
+    currLine.style.transform = `rotate(${i}deg) translateY(${far}px)`;
+    windCircle.appendChild(currLine);
+    if (i % 45 == 0) {
+      currLine.classList.add("bold");
+    }
+  }
+}
+
+function checkIfLineShouldBePlaced(i) {
+  return (
+    (i < 100 && i > 80) ||
+    (i < 190 && i > 170) ||
+    (i < 280 && i > 260) ||
+    (i < 370 && i > 350) ||
+    i < 10
+  );
+}
+
+function makeLine() {
+  const compassLine = document.createElement("div");
+  compassLine.classList.add("compass-line");
+
+  return compassLine;
+}

d2a343a015e07759f318dd6d2413f16ec575cbf6
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2025-10-14 10:04:53 -0400

    Add feature that colors temperature range's gradient based on min temp and max temp.

--- src/dom/createDayElement.js
+++ src/dom/createDayElement.js
@@ -1,5 +1,72 @@
 import { setWeatherIcon } from "../helpers/icon";
 
+function changeGradient(minTemp, maxTemp, coloredLine) {
+  let startGradient = "";
+  let endGradient = "";
+
+  if (maxTemp >= 90) {
+    //red
+    endGradient = "#ff3d3d";
+  } else if (maxTemp >= 80) {
+    //saturated orange
+    endGradient = "#FF5B03";
+  } else if (maxTemp >= 75) {
+    //orange
+    endGradient = "#ff9b38ff";
+  } else if (maxTemp >= 70) {
+    //lighter orange
+    endGradient = "#FFCC26";
+  } else if (maxTemp >= 65) {
+    //yellow
+    endGradient = "#fff426ff";
+  } else if (minmaxTempTemp >= 60) {
+    //green
+    endGradient = "#D2E864";
+  } else if (maxTemp >= 50) {
+    //green blue
+    endGradient = "#91E3B3";
+  } else if (maxTemp >= 40) {
+    //light blue
+    endGradient = "#96f5ebff";
+  } else {
+    //bright blue
+    endGradient = "#07E5ED";
+  }
+
+  if (minTemp >= 90) {
+    //red
+    startGradient = "#ff3d3d";
+  } else if (minTemp >= 80) {
+    //saturated orange
+    startGradient = "#FF5B03";
+  } else if (minTemp >= 75) {
+    //orange
+    startGradient = "#ff9b38ff";
+  } else if (minTemp >= 70) {
+    //lighter orange
+    startGradient = "#FFCC26";
+  } else if (minTemp >= 65) {
+    //yellow
+    startGradient = "#fff426ff";
+  } else if (minTemp >= 60) {
+    //green
+    startGradient = "#D2E864";
+  } else if (minTemp >= 50) {
+    //green blue
+    startGradient = "#91E3B3";
+  } else if (minTemp >= 40) {
+    //light blue
+    startGradient = "#96f5ebff";
+  } else {
+    //bright blue
+    startGradient = "#07bbedff";
+  }
+
+  console.log(endGradient + " " + startGradient);
+
+  coloredLine.style.background = `linear-gradient(90deg,${startGradient} 0%, ${endGradient} 100%)`;
+}
+
 export function createDayElement(currentDay, min, max) {
   console.log(currentDay);
   const date = currentDay.date;
@@ -41,6 +108,9 @@ export function createDayElement(currentDay, min, max) {
     -50 + width / 2 + start * (100 / (max - min))
   }px)`;
   coloredLine.style[`width`] = `${width}px`;
+
+  //Change the color of the gradient depending on its min temperature.
+  changeGradient(minTemp, maxTemp, coloredLine);
 
   dayItem.appendChild(dailyTitle);
   dayItem.appendChild(dailyIcon);
